2fe89718a1eecb74f5fb93b062ca9b8c7463bda0
Jared Short <jaredlshort@gmail.com>
2021-05-03 15:21:48 +0000

    feat: init

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { onMount } from 'svelte';
+  import store from './store.js';
+  let messages = [];
+
+  onMount(() => {
+    store.subscribe((currentMessage) => {
+      messages = [...messages, currentMessage];
+    });
+  });
+</script>
+
+<main>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
+  {#each messages as message, i}
+    <p>{message}</p>
+  {/each}
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
